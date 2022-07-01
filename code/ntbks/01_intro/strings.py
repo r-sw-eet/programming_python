6f09fb7337191f81a705ae22ef71c132cc1ee388
Ralf <suess.ralf@posteo.de>
2022-07-01 17:17:06 +0200

    reformatting according to PEP8

--- code/ntbks/01_intro/strings.py
+++ code/ntbks/01_intro/strings.py
@@ -4,11 +4,11 @@ s1 = 'Python is a programming language and an animal'
 
 s2 = "this is a snake and it is " + str(5) + " metres long"
 
-s3 = 'that is a ' + 'very '*3 + 'long animal'
+s3 = 'that is a ' + 'very ' * 3 + 'long animal'
 
-print (s1)
-print (s2)
-print (s3)
+print(s1)
+print(s2)
+print(s3)
 
 s4 = 'This'
 s5 = 'This'
@@ -19,8 +19,7 @@ print(s4 == s6)
 print(s4 is s5)
 print(s4 is s4)
 
-
-a = [1,2,3,4,5]
+a = [1, 2, 3, 4, 5]
 b = a
 print('b is a? ' + str(b is a))
 print('b is a? ' + str(b == a))
@@ -29,37 +28,34 @@ b = a[:]
 print('b is a? ' + str(b is a))
 print('b is a? ' + str(b == a))
 
-#Python caches small integer and string objects, therefore is sometimes holds true for small values
-#cf. slides
+# Python caches small integer and string objects, therefore is sometimes holds true for small values
+# cf. slides
 print('caching example:')
-print(10000 == 10**4)
-print(10000 is 10**4)
+print(10000 == 10 ** 4)
+print(10000 is 10 ** 4)
 
 print('a' is 'a')
-print('aa' is 'a'*2)
+print('aa' is 'a' * 2)
 x = 'a'
 print('aa' is x * 2)
 print('aa' is sys.intern(x * 2))
 
+# isinstance(s1, str)
+# if type(s1) == str
 
-#isinstance(s1, str)
-#if type(s1) == str
+# print ( 'The data file is ' + 5 + ' MB large') -- produces error
+print('The data file is ' + str(5) + ' MB large')
 
-#print ( 'The data file is ' + 5 + ' MB large') -- produces error
-print ( 'The data file is ' + str(5) + ' MB large')
-
-
-print ( '\nresult of intern experiment:')
-stringA = sys.intern('myString'*1000)
-stringB = sys.intern('myString'*1000)
+print('\nresult of intern experiment:')
+stringA = sys.intern('myString' * 1000)
+stringB = sys.intern('myString' * 1000)
 
 print(stringA is stringB)
 
-stringC = 'myNewString'*1000
-stringD = 'myNewString'*1000
+stringC = 'myNewString' * 1000
+stringD = 'myNewString' * 1000
 
 print(stringD is stringC)
 
-
-#user_input = input("Enter a message... ")
-#print('User entered: ' + user_input)
+# user_input = input("Enter a message... ")
+# print('User entered: ' + user_input)
--- code/ntbks/01_intro/strings_2.py
+++ code/ntbks/01_intro/strings_2.py
@@ -10,42 +10,40 @@ stringC = 'abcdefghijkl'
 print(stringC[::])
 print(stringC[::2])
 
-#stringA[0] = 'A' --> this leads to an error
+# stringA[0] = 'A' --> this leads to an error
 print(id(stringA))
 
-
 print('\nidentity example')
-#identity example with a float
+# identity example with a float
 f = 7
 print(id(f))
 f = 8
 print(id(f))
 
-d1 =  'data'
+d1 = 'data'
 print(id(d1))
 d1 += ' science'
 print(id(d1))
 
 print(d1)
 
-l1 = [1,2,3]
+l1 = [1, 2, 3]
 print(id(d1))
-l1 += [4,5,6]
+l1 += [4, 5, 6]
 print(id(d1))
-print (l1)
+print(l1)
 
-
-l2 = [1,2,3]
+l2 = [1, 2, 3]
 l3 = l2
 
-l3 += [4,5]
+l3 += [4, 5]
 print(l2)
 print(l3)
 
 print(id(l2))
 print(id(l3))
 
-#immutable objects
+# immutable objects
 
 print('\nimmutable objects:')
 s1 = 'this '
@@ -64,11 +62,10 @@ print(s2 is s1)
 print(id(s1))
 print(id(s2))
 
-
-#changing values in containers
+# changing values in containers
 
 print('\ncontainer example')
-data = [1,2,3,4,5]
+data = [1, 2, 3, 4, 5]
 tup = ('someString', data)
 
 print(data)
@@ -79,12 +76,12 @@ data[3] = 'change'
 print(data)
 print(id(tup))
 
-#special case - updating values of immutable objects in an immutable container
+# special case - updating values of immutable objects in an immutable container
 
 print('\nspecial case\n')
 str1 = 'myString'
 int1 = 25
-tup1 = ('a','b','c')
+tup1 = ('a', 'b', 'c')
 tup2 = (str1, int1, tup1)
 
 print('id str1:' + str(id(str1)))
@@ -92,7 +89,6 @@ print('id int1:' + str(id(int1)))
 print('id tup1:' + str(id(tup1)))
 print(tup2)
 print('id tup2:' + str(id(tup2)))
-
 
 str1 += ' has changed'
 int1 = 500
